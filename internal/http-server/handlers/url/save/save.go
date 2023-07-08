c3987f66469a8d0769add18521adb9023520be95
aaa2ppp <aleksandr@panfilov.pp.ru>
2023-07-08 03:15:17 +0300

    fix unlocal log variable in handlers and middleware

--- internal/http-server/handlers/url/save/save.go
+++ internal/http-server/handlers/url/save/save.go
@@ -38,7 +38,7 @@ func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		const op = "handlers.url.save.New"
 
-		log = log.With(
+		log := log.With(
 			slog.String("op", op),
 			slog.String("request_id", middleware.GetReqID(r.Context())),
 		)

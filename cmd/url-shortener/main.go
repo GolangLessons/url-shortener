18c5d588d8c618bafca80f8fb9adee09c106f50a
Pavel Filimonenkov <pafilimo10@mts.ru>
2023-07-15 22:19:49 +0300

    Improve pretty logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -86,33 +86,24 @@ func main() {
 }
 
 func newLogger(env string) *slog.Logger {
-	var log *slog.Logger
-
 	switch env {
 	case envLocal:
-		log = newPrettySlog()
+		return slog.New(slogpretty.NewHandler().
+			WithLevel(slog.LevelDebug).
+			WithLevelEmoji().
+			WithAddSource().
+			WithTimeLayout("15:04:05"))
 	case envDev:
-		log = slog.New(
+		return slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
 		)
 	case envProd:
-		log = slog.New(
-			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
-		)
-	default: // If env config is invalid, set prod settings by default due to security
-		log = slog.New(
+		return slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
 		)
 	}
-
-	return log
-}
-
-func newPrettySlog() *slog.Logger {
-	handler := slogpretty.NewHandler().
-		WithLevel(slog.LevelDebug).
-		WithLevelEmoji().
-		WithAddSource().
-		WithTimeLayout("15:04:05")
-	return slog.New(handler)
+	// If env config is invalid, set prod settings by default due to security
+	return slog.New(
+		slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+	)
 }
